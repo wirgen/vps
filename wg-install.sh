7da42f3837bc43956d7524b220b8771f10ed2199
Denis Nikiforov <wirgen@users.noreply.github.com>
2020-11-02 13:51:28 +0300

    Update wg-install.sh

--- wg-install.sh
+++ wg-install.sh
@@ -11,7 +11,13 @@ cd wg-quick
 wget https://raw.githubusercontent.com/burghardt/easy-wg-quick/master/easy-wg-quick
 chmod +x easy-wg-quick
 sed -i '/^DNS =.*/i MTU = 1500' easy-wg-quick
+echo "3785" > portno.txt
+echo "1.1.1.1" > intnetdns.txt
 ./easy-wg-quick
+
+SEQNO=$(cat seqno.txt)
+CURRENT=$((SEQNO-1))
+qrencode -o "wgclient_$1.png" < "wgclient_$1.conf"
 
 ln -s $(pwd)/wghub.conf /etc/wireguard/
 systemctl enable wg-quick@wghub
