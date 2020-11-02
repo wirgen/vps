08dbcf3d3a8b30ce77871ec3d3aef1d9e12a5769
Denis Nikiforov <wirgen@users.noreply.github.com>
2020-11-02 13:52:08 +0300

    Update wg-new.sh

--- wg-new.sh
+++ wg-new.sh
@@ -3,5 +3,9 @@
 cd wg-quick
 ./easy-wg-quick
 
+SEQNO=$(cat seqno.txt)
+CURRENT=$((SEQNO-1))
+qrencode -o "wgclient_$CURRENT.png" < "wgclient_$CURRENT.conf"
+
 systemctl restart wg-quick@wghub
 wg show
