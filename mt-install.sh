2edcad0747290a2f8042aa1e021209353e8576d6
Denis Nikiforov <wirgen@users.noreply.github.com>
2020-11-02 10:48:31 +0300

    Create mt-install.sh

---
+++ mt-install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+apt update
+apt install -y git curl build-essential libssl-dev zlib1g-dev
+
+rm -rf MTProxy
+git clone https://github.com/TelegramMessenger/MTProxy.git
+cd MTProxy/
+make
+
+cp objs/bin/mtproto-proxy /usr/bin/
+chmod 777 /usr/bin/mtproto-proxy
+cd /etc
+rm -rf mtproto-proxy
+mkdir mtproto-proxy
+cd mtproto-proxy
+curl -s https://core.telegram.org/getProxySecret -o proxy-secret
+curl -s https://core.telegram.org/getProxyConfig -o proxy-multi.conf
+
+IP=$(wget -qO- ipinfo.io/ip)
+PORT=8443
+SECRET=$(head -c 16 /dev/urandom | xxd -ps)
+
+cat > "/etc/systemd/system/mtproto-proxy.service" << EOF
+[Unit]
+Description=MTProxy
+After=network.target
+[Service]
+ExecStart=/usr/bin/mtproto-proxy -u nobody -p 6419 -H $PORT -S $SECRET --aes-pwd /etc/mtproto-proxy/proxy-secret /etc/mtproto-proxy/proxy-multi.conf -M 1
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl start mtproto-proxy
+systemctl enable mtproto-proxy
+
+cp -f mt-update.sh /etc/cron.daily/mt-update
+
+qrencode -t ansiutf8 "tg://proxy?server=$IP&port=$PORT&secret=$SECRET"
+echo "With DD in secret"
+qrencode -t ansiutf8 "tg://proxy?server=$IP&port=$PORT&secret=dd$SECRET"
+echo "IP: $IP"
+echo "PORT: $PORT"
+echo "SECRET: $SECRET"
