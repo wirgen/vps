bffa82e23cb744681dd891f8dd61a3122ef0ec0b
Denis Nikiforov <wirgen@users.noreply.github.com>
2020-11-03 13:36:00 +0300

    Update ss-update-cert.sh

--- ss-update-cert.sh
+++ ss-update-cert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-iptables -D INPUT -p tcp --dport 80 -j REJECT
+iptables -D INPUT -p tcp --dport 80 -j DROP
 
 certbot certonly -d $DOMAIN --cert-name ss --quiet --webroot -w /var/www/letsencrypt/
 systemctl reload nginx
 
-iptables -A INPUT -p tcp --dport 80 -j REJECT
+iptables -A INPUT -p tcp --dport 80 -j DROP
 iptables-save
