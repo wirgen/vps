fe32946779cd79d8f9dfc8c3a4d53e508fd14cee
Denis Nikiforov <wirgen@users.noreply.github.com>
2020-11-03 13:26:37 +0300

    Update wg-install.sh

--- wg-install.sh
+++ wg-install.sh
@@ -3,6 +3,10 @@
 apt update
 apt install -y wireguard-tools mawk grep iproute2 qrencode
 
+PORT=3785
+DNS="1.1.1.1"
+MTU=1500
+
 rm /etc/wireguard/wghub.conf
 rm -rf wg-quick
 
@@ -10,16 +14,16 @@ mkdir wg-quick
 cd wg-quick
 wget https://raw.githubusercontent.com/burghardt/easy-wg-quick/master/easy-wg-quick
 chmod +x easy-wg-quick
-sed -i '/^DNS =.*/i MTU = 1500' easy-wg-quick
-echo "3785" > portno.txt
-echo "1.1.1.1" > intnetdns.txt
+sed -i "/^DNS =.*/i MTU = $MTU" easy-wg-quick
+echo $PORT > portno.txt
+echo $DNS > intnetdns.txt
 ./easy-wg-quick
-
-SEQNO=$(cat seqno.txt)
-CURRENT=$((SEQNO-1))
-qrencode -o "wgclient_$1.png" < "wgclient_$1.conf"
 
 ln -s $(pwd)/wghub.conf /etc/wireguard/
 systemctl enable wg-quick@wghub
 systemctl start wg-quick@wghub
 wg show
+
+SEQNO=$(cat seqno.txt)
+CURRENT=$((SEQNO-1))
+qrencode -o "wgclient_$CURRENT.png" < "wgclient_$CURRENT.conf"
