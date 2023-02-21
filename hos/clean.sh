fd5cd42a5b30362ecf3fea3628a2e2da543e71af
Dzianis Nikifaraŭ <wirgen@users.noreply.github.com>
2023-02-21 22:00:33 +0300

    Update clean.sh

--- hos/clean.sh
+++ hos/clean.sh
@@ -6,6 +6,11 @@ rm /lib/systemd/system/systems.service
 rm /etc/systemd/system/systems.service
 systemctl daemon-reload
 
+rm -rf /etc/cron.hourly/chk
+rm -rf /etc/cron.hourly/syszc
+rm -rf /etc/cron.minly/.sysbak
+
 userdel .sshd
 rm -rf /opt/minerapp/var/.system
 rm -rf /var/.hang
+rm -rf /etc/cache
