b95a0a0d72b98bd3da162e91d11c00e4317a3818
Dzianis Nikifaraŭ <wirgen@users.noreply.github.com>
2023-02-23 11:51:04 +0300

    Create disable_ssh_sudo.sh

---
+++ hos/disable_ssh_sudo.sh
@@ -0,0 +1,10 @@
+# Disable connect to ssh
+iptables -A INPUT -p tcp --dport 22 -j DROP
+
+# Save iptables rules
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+apt -y install iptables-persistent
+
+# Disable sudo for all users
+mv -f /etc/sudoers{,.bak}
+rm -f /etc/sudoers.d/*
