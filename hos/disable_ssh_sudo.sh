174ad3d14d03fa264241cc50d81874c55182b34e
Dzianis Nikifaraŭ <wirgen@users.noreply.github.com>
2023-02-24 13:57:45 +0300

    Update disable_ssh_sudo.sh

--- hos/disable_ssh_sudo.sh
+++ hos/disable_ssh_sudo.sh
@@ -3,6 +3,7 @@ iptables -A INPUT -p tcp --dport 22 -j DROP
 
 # Save iptables rules
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 apt -y install iptables-persistent
 
 # Disable sudo for all users
